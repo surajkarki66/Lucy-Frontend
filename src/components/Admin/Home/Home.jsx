2a36566c8b7ffd5e5cfa3a8919d67a8998fa1b79
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-09-17 17:00:36 +0545

    feat: added admin home

---
+++ src/components/Admin/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
+import SideBar from "../SideBar/SideBar";
+import NavBar from "../NavBar/NavBar";
+import Tables from "../Table/Table";
+import './Home.css'
+import { ADMINLOGIN } from "../../Constants/RoutesConstants";
+
+const AdminPageHome = () => {
+  const navigate = useNavigate();
+  const logoutButtonHandler = () => {
+          localStorage.removeItem('token')
+          localStorage.removeItem('user')
+          navigate(ADMINLOGIN);
+  };
+  const [content, setContent] = useState('Feedbacks')
+
+console.log(content)
+  return (
+    <div className="adminhome">
+            <SideBar logoutButtonHandler={logoutButtonHandler} setContent={setContent}/>
+            <div className="homeContainer">
+                <NavBar />
+                  <Tables content={content}/>
+            </div>
+        </div>
+  );
+};
+
+export default AdminPageHome;
