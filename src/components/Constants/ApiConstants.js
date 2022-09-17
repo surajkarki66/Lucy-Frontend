3690161e49deebb717bb8f2a41f841b4d4986a7a
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-09-17 17:01:58 +0545

    fix: added  routes and api constatns

---
+++ src/components/Constants/ApiConstants.js
@@ -0,0 +1,13 @@
+export const API = 'https://rightpathapi.herokuapp.com/api'
+
+export const VERSION = '/v2'
+
+export const ADMINLOGINAPI = API + VERSION + '/auth/login'
+
+export const DOCUMENTAPI = API + VERSION + '/document'
+
+export const SIGNUPAPI = API + VERSION + '/user/signup'
+
+export const LOGINAPI = API + VERSION + 'user/login'
+
+export const CREATERESPONSE = API + VERSION + 'response/create'
