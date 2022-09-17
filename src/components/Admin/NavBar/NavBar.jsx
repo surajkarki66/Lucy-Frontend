1d8dbe813fb03b54d8419dc68ddf97ecc676aa3f
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-09-17 17:00:54 +0545

    feat: added admin navbar

---
+++ src/components/Admin/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import './NavBar.css'
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+
+const NavBar = () => {
+    return (
+        <div className="navbar">
+          <div className="nav-wrapper">
+            <div className="search">
+              <input type="text" placeholder="Search..." />
+              <SearchOutlinedIcon />
+            </div>
+            <div className="items">
+              <div className="item">
+                <LanguageOutlinedIcon className="icon" />
+                English
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+}
+export default NavBar;
