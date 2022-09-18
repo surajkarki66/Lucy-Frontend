0f3f6776256973a851d449221d39d9ed6dff5957
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-09-18 15:13:43 +0545

    feat: added token as header in axious instance

--- src/axios-url.ts
+++ src/axios-url.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const token = localStorage.getItem('tokan')
+
+axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
 const instance = axios.create({
-  baseURL: "http://0.0.0.0:8080/api/v2",
+  baseURL: "http://localhost:8080/api/v2",
   withCredentials: true,
 });
 
