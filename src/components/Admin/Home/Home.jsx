5043e79fbb5c93c838cf68d4efa75ce5cd7e5455
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-09-18 15:14:15 +0545

    feat: api integration and auth checked

--- src/components/Admin/Home/Home.jsx
+++ src/components/Admin/Home/Home.jsx
@@ -1,20 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
 import SideBar from "../SideBar/SideBar";
 import NavBar from "../NavBar/NavBar";
 import Tables from "../Table/Table";
+import jwt from 'jwt-decode'
+
 import './Home.css'
 import { ADMINLOGIN } from "../../Constants/RoutesConstants";
+import Spinner from "../../Loaders/Spinner";
 
 const AdminPageHome = () => {
   const navigate = useNavigate();
   const logoutButtonHandler = () => {
-          localStorage.removeItem('token')
-          localStorage.removeItem('user')
+          localStorage.removeItem('tokan')
           navigate(ADMINLOGIN);
+          window.location.reload();
   };
   const [content, setContent] = useState('Feedbacks')
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem('tokan')
+    let user;
+    if(token) user = jwt(token)
+    console.log(user)
+    if(!user?.user_id) navigate(ADMINLOGIN)
+    setIsLoading(false)
+  }, [])
+
+  if(isLoading) return <Spinner />
 
   // Check if user in cookies or localstorage if not then navigate login page
   return (
